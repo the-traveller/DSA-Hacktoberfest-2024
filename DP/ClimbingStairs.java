b38601de52743fdd8f852ec9bf566f42720698d6
adityadev31 <52311859+adityadev31@users.noreply.github.com>
2024-10-20 21:51:58 +0530

    Added Memoize code

--- DP/ClimbingStairs.java
+++ DP/ClimbingStairs.java
@@ -31,7 +31,17 @@ class Recursion implements Solution {
 
 // ===== Memoization =====
 class Memoize implements Solution {
-  // implement
+  public int ans(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return climbStairs(n, dp);
+    }
+
+    private int climbStairs(int n, int[] dp) {
+        if(n <= 1) return dp[n] = 1;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = climbStairs(n-1, dp) + climbStairs(n-2, dp);
+    }
 }
 
 // ===== DP =====
